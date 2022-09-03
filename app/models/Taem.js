5df35cec22eec33498838b3e96276dbe309e9403
Alireza <alirezatlb45@gmail.com>
2022-09-04 01:34:04 +0430

    added image field to Team model

--- app/models/Taem.js
+++ app/models/Taem.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose");
 
 const TeamSchema = new mongoose.Schema({
+    name : { type : String, required : true, trim : true },
+    desctription : { type : String, required : true, trim : true },
     captain : { type : mongoose.Types.ObjectId, required : true },
     members : { type : [mongoose.Types.ObjectId], default : [] },
     username : { type : String, required : true, trim : true, unique : true },
-    name : { type : String, required : true, trim : true },
-    desctription : { type : String, required : true, trim : true }
+    profile_image : { type : String, required : true, trim : true }
 }, { timestamps : true } );
 
 const TeamModel = mongoose.model("Team", TeamSchema);
