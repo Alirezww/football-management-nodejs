8e7cc98bad3544670e4d85d137d200f3f298eb39
Alireza <alirezatlb45@gmail.com>
2022-09-04 01:30:35 +0430

    added match mongoose schema

---
+++ app/models/Match.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const MatchSchema = new mongoose.Schema({
+    name : { type : String, required : true, trim : true },
+    stadium : { type : String, required : true, trim : true },
+    date : { type : Date, required : true },
+    status : { type : String, enum : ["playing", "canceled", "active"], default : "active" },
+    teams : { type : [mongoose.Types.ObjectId], required : true, maxLength : 2, minLength :2 },
+    judges : { type : [mongoose.Types.ObjectId], required : true }
+}, { timestamps : true } );
+
+const MatchModel = mongoose.model("Match", MatchSchema);
+
+module.exports = MatchModel
