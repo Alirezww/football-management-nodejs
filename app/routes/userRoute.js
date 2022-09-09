adc416a33b2bb2c77d398fcae2fff8fcf6982342
Alireza <alirezatlb45@gmail.com>
2022-09-09 19:32:14 +0430

    added swagger for profile route

--- app/routes/userRoute.js
+++ app/routes/userRoute.js
@@ -1,8 +1,38 @@
 const { UserController } = require("../http/controllers/user.controller");
+const { isAuthenticated } = require("../http/middlewares/isAuthenticated");
 
 const router = require("express").Router();
 
-router.get("/", UserController.profile)
+/**
+ * @swagger
+ * tags:
+ *      name: profile
+ *      description: all actions for user profile
+ */
+
+/**
+ * @swagger
+ * /profile/:
+ *      get:
+ *          summary: all data about logged in user
+ *          tags: [profile]
+ *          parameters:
+ *              -   name: authorization
+ *                  in: header
+ *                  type: string
+ *                  required: true
+ * 
+ *          responses:
+ *              200:
+ *                  description: success
+ *              404:
+ *                  description: notFound
+ *              401:
+ *                  description: unAuthorized
+ *              500:
+ *                  description: InternalErrorServer
+ */
+router.get("/", isAuthenticated ,UserController.profile)
 
 module.exports = {
     UserRoute : router
