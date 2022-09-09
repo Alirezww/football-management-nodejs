ba5e4379eb87f7cac1fa8fb2e87504ece174fe3b
Alireza <alirezatlb45@gmail.com>
2022-09-09 19:01:42 +0430

    added timeLeft field to otpSchema

--- app/models/User.js
+++ app/models/User.js
@@ -9,7 +9,8 @@ const InviteRequest = new mongoose.Schema({
 const otpSchema = new mongoose.Schema({
     code : { type: Number, default: 0 },
     expiresIn: { type: Date, default: 0 },
-    times : { type: Number, default: 0 }
+    times : { type: Number, default: 0 },
+    timeLeft: { type: Number, deafult: 0 }
 })
 
 const userSchama = new mongoose.Schema({
